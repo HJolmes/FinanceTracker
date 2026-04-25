97fcbf8770949deeb697b01738edf78d8d602b36
HJolmes <h.jolmes@jolmes.de>
2026-04-25 04:12:26 +0200

    feat: register service worker for PWA install prompt

--- src/index.js
+++ src/index.js
@@ -4,3 +4,11 @@ import App from "./App";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(<React.StrictMode><App /></React.StrictMode>);
+
+if ("serviceWorker" in navigator) {
+  window.addEventListener("load", () => {
+    navigator.serviceWorker
+      .register(process.env.PUBLIC_URL + "/service-worker.js")
+      .catch(() => {});
+  });
+}
